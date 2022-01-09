42f4f6db2f5188b221a94815497a8416a52f3b24
AndreiBucur <51850157+AndreiBucur@users.noreply.github.com>
2022-01-09 15:18:49 +0200

    try

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 	const getVideo = () => {
 		let video = videoRef.current;
 		if (navigator.mediaDevices.getUserMedia) {
-			navigator.mediaDevices.getUserMedia({ video: true })
+			navigator.mediaDevices.getUserMedia({ video: {facingMode: {exact: 'environment'}}})
 			  .then(function (stream) {
 				video.srcObject = stream;
 				video.play();
